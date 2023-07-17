054d4d7b84af81b5c2d7a72a3a58a6d752a32504
Vanshul <vanshuldahiya2002@gmail.com>
2023-07-17 15:16:01 +0530

    fixed camera functionality for different buttons in table

--- src/app/table/table.page.ts
+++ src/app/table/table.page.ts
@@ -2,24 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Capacitor, Plugins } from '@capacitor/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import {CameraPhoto, CameraResultType , CameraSource }  from '@capacitor/camera';
-import { FilesystemDirectory}  from '@capacitor/filesystem';
+import { CameraPhoto, CameraResultType, CameraSource } from '@capacitor/camera';
+import { FilesystemDirectory } from '@capacitor/filesystem';
 import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
-const { Camera , Filesystem } = Plugins;
+const { Camera, Filesystem } = Plugins;
 
 export interface PeriodicElement {
   course: string;
   checkbox: boolean;
   selectOption: string;
   image: any | undefined;
-} 
+}
 const ELEMENT_DATA: PeriodicElement[] = [
-  { course: 'B.Pharma', checkbox: false, selectOption: '',image: undefined},
-  { course: 'M.Phil', checkbox: false, selectOption: '',image: undefined},
+  { course: 'B.Pharma', checkbox: false, selectOption: '', image: undefined },
+  { course: 'M.Phil', checkbox: false, selectOption: '', image: undefined },
 
-  
-  { course: 'M.Pharma', checkbox: false, selectOption: '',image: undefined},
+  { course: 'M.Pharma', checkbox: false, selectOption: '', image: undefined },
 ];
 @Component({
   selector: 'app-table',
@@ -27,26 +26,31 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./table.page.scss'],
 })
 export class TablePage implements OnInit {
-  selectedOption : any;
-  selectedOption2 : any;
-  imageSource:any;
+  selectedOption: any;
+  selectedOption2: any;
+  imageSource: any;
   form: FormGroup;
   dataSource: PeriodicElement[] | undefined = [];
-  constructor(private route : ActivatedRoute , private domSanitizer:DomSanitizer,private http : HttpClient){
+  constructor(
+    private route: ActivatedRoute,
+    private domSanitizer: DomSanitizer,
+    private http: HttpClient
+  ) {
     this.form = new FormGroup({
-      image: new FormControl('')
+      image: new FormControl(''),
     });
-  
   }
   ngOnInit(): void {
-    this.getData()
+    this.getData();
     // throw new Error('Method not implemented.');
   }
   async getData() {
     try {
-      const data = await this.http.get<PeriodicElement[]>('../../assets/data.json').subscribe(data => {
-        this.dataSource = data;
-      })
+      const data = await this.http
+        .get<PeriodicElement[]>('../../assets/data.json')
+        .subscribe((data) => {
+          this.dataSource = data;
+        });
       // this.dataSource = data;
     } catch (error) {
       console.error('Error retrieving data:', error);
@@ -55,65 +59,60 @@ export class TablePage implements OnInit {
   displayedColumns: string[] = ['course', 'yesNo', 'ifYes', 'inspectorRemark'];
 
   separateDataSource: PeriodicElement[] = [];
-  
+
   photoData: string | undefined;
-  
-  
-  takePicture = async () => {
+
+  takePicture = async (index: number) => {
     const image = await Camera['getPhoto']({
       quality: 90,
-      allowEditing:false,
+      allowEditing: false,
       resultType: CameraResultType.Uri,
-      source:CameraSource.Prompt
-      
-    }); 
+      source: CameraSource.Prompt,
+    });
 
     // this.imageSource= 'data:image/jpeg;base64,'+image.base64String;
     // console.log(this.imageSource)
-    this.imageSource=this.domSanitizer.bypassSecurityTrustUrl(image.webPath ? image.webPath : "");
-    console.log(this.imageSource)
-
-  }
-  getPhoto(){
-    return this.imageSource;
-    
+    this.imageSource = this.domSanitizer.bypassSecurityTrustUrl(
+      image.webPath ? image.webPath : ''
+    );
+    if (this.dataSource && this.dataSource[index] && this.imageSource) {
+      this.dataSource[index].image = this.imageSource;
+    }
+    console.log(this.imageSource);
+  };
+  getPhoto(index:number) {
+    if (this.dataSource && this.dataSource[index] && this.imageSource)
+    return this.dataSource[index].image;
   }
   // updateCheckboxValue(event: Event, index: number) {
   //   const target = event.target as HTMLInputElement;
   //   this.dataSource[index].checkbox = target.checked ;
   //   // this.dataSource[index].checkbox = checked;
   // }
-  
+
   submitTableData(){
     localStorage.setItem('tableData', JSON.stringify(this.dataSource));
   }
   getTableData(){
-    const storedData= 
+    const storedData=
     localStorage.getItem('tableData');
     if(storedData){
      const retrievedData = JSON.parse(storedData)
      this.separateDataSource=retrievedData;
      console.log(storedData);
-     
+
     }
   }
-  
 }
-  
-  
-  
-  
-  
-  
-  function updateCheckboxValue(event: Event | undefined, Event: { new(type: string, eventInitDict?: EventInit | undefined): Event; prototype: Event; readonly NONE: 0; readonly CAPTURING_PHASE: 1; readonly AT_TARGET: 2; readonly BUBBLING_PHASE: 3; }, index: any, number: any) {
-    throw new Error('Function not implemented.');
-  }
-  
-  function submitTableData() {
-    throw new Error('Function not implemented.');
-  }
-  
-  function getTableData() {
-    throw new Error('Function not implemented.');
-  }
 
+// function updateCheckboxValue(event: Event | undefined, Event: { new(type: string, eventInitDict?: EventInit | undefined): Event; prototype: Event; readonly NONE: 0; readonly CAPTURING_PHASE: 1; readonly AT_TARGET: 2; readonly BUBBLING_PHASE: 3; }, index: any, number: any) {
+//   throw new Error('Function not implemented.');
+// }
+
+// function submitTableData() {
+//   throw new Error('Function not implemented.');
+// }
+
+// function getTableData() {
+//   throw new Error('Function not implemented.');
+// }
