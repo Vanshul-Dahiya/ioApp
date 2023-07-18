6c3526a7398da4a28d4a99bbd0dc61ad8dc827f9
Vanshul <vanshuldahiya2002@gmail.com>
2023-07-18 11:53:30 +0530

    footer fixed

--- src/app/table/table.page.spec.ts
+++ src/app/table/table.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { TablePage } from './table.page';
 
 describe('TablePage', () => {
